d6c5c348abc56e84b98174506c225fedf4911256
yongj0 <109091692+yongj0@users.noreply.github.com>
2022-07-12 12:25:26 +0900

    2022.07.12

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,11 +10,22 @@ public class UIManager : MonoBehaviour
 
     private void Update()
     {
-        if(Input.anyKeyDown)
+        if (Input.anyKeyDown)
         {
             buttonPanel.SetActive(true);
+
+            StartCoroutine(UIFunction.Instance.MoveUI(buttonPanel, buttonPanel.transform.position, new Vector3(960.0f,540.0f,0.0f)));
+
         }
     }
 
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
 
+    public void GameQuit()
+    {
+        Application.Quit(); 
+    }
 }
